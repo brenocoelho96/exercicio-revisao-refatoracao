1ef4deac522b35618a13814893016abfcfd6beea
brenocoelho96 <73148133+brenocoelho96@users.noreply.github.com>
2020-10-20 18:33:06 -0300

    Update Vendedor.cpp

--- Vendedor.cpp
+++ Vendedor.cpp
@@ -1,14 +1,27 @@
 #include <string>
-#include "Empregado.hpp"
+#include "Vendedor.hpp"
 
 class Vendedor : public Empregado {
 
-  public:
-    std::string nome;  	  
-	
-	double quotaTotalAnual() {
-	  return quotaMensalVendas * 12;
-	}
+   Vendedor::Vendedor (std::string _Nome, double _SalarioHora, double _HorasTrabalhadas, int _QuotaMensal){
+     this->Nome = Nome;
+     this->SalarioHora = _SalarioHora;
+     this->HorasTrabalhadas = _HorasTrabalhadas;
+     this->QuotaMensal = _QuotaMensal;
+   }
+
+   	double Vendedor::GetQuotaTotalAnual(){
+     double ValorFinal = this->QuotaMensal * 12 ;
+     return ValorFinal;
+ 	}
+
+ 	void Vendedor::GetVendedor ()  {
+        std::cout << "Nome: " << this->Nome << std::endl;
+        std::cout << "Salario Mes: " << this->GetPagamentoPorMes() << std::endl;
+        std::cout << "Quota vendas: " << this->GetQuotaTotalAnual() << std::endl;
+ 	}
 	
 };
+
+
 
