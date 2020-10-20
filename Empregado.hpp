50d52ba3faadd09c62c5dab107ab212ee154ca14
brenocoelho96 <73148133+brenocoelho96@users.noreply.github.com>
2020-10-20 18:45:01 -0300

    Update Empregado.hpp

--- Empregado.hpp
+++ Empregado.hpp
@@ -1,18 +1,22 @@
-#include "Empregado.hpp"
+#ifndef EMPREGADO_H
+#define EMPREGADO_H
 
-    Empregado::Empregado(std::string _Nome, double _SalarioHora, double _TrabalhoHora) {
-       this->Nome = _Nome;
-       this->SalarioHora = _SalarioHora;
-       this->TrabalhoHora = _TrabalhoHora; 
-    }
+#include <iostream>
+#include <string>
 
+class Empregado {
 
-    double Empregado::GetPagamentoPorMes(){
-        double TotalHoras = this->TrabalhoHora;
+  public:
+  Empregado();
+  ~Empregado();
+  double PagamentoMes();
+  void MostrarInfo();
+  
+	protected:
+  std::string Nome;
+  double SalarioHora;
+  double TrabalhoHora;
+	
+};
 
-        if (this->TrabalhoHora > 8) {
-        double aux = this->TrabalhoHora - 8;
-        TotalHoras += aux / 2;
-        }
-        return  TotalHoras * this->SalarioHora;
-    }
+#endif
